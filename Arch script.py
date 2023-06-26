cb036e803ea56b716b9f5bb0f3b010d7ae518945
lwrlock <137615326+lwrlock@users.noreply.github.com>
2023-06-26 14:01:25 +0300

    Create Arch script.py

---
+++ Arch script.py
@@ -0,0 +1,108 @@
+from os import system, chdir
+import readline
+import time
+
+aur_helper = input("what is your aur helper? = ")
+
+system("clear")
+
+from colorama import Fore, Back, Style
+print(Fore.RED + "what is your desktop enviroment?")
+print(Style.RESET_ALL)
+
+de = input("gnome , gnome debloated , xfce4 , kde plasma , kde plasma minimal = ")
+
+if de == "gnome":
+          system("sudo pacman -S gnome xorg xorg-xinit")
+          input("press enter to continue")
+
+if de == "gnome debloated":          
+          system("sudo pacman -S eog evince file-roller gnome-backgrounds gnome-characters gnome-color-manager gnome-terminal gnome-control-center gnome-disk-utility gnome-menus gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-text-editor gnome-tweaks gvfs gvfs-afc gvfs-mtp nautilus xdg-user-dirs-gtk xdg-desktop-portal xdg-desktop-portal-gnome xdg-desktop-portal-gtk xdg-user-dirs xdg-utils xorg xorg-xinit")
+          input("press enter to continue")
+
+if de == "xfce4":
+          system("sudo pacman -S xfce4 xfce4-goodies xorg xorg-xinit")
+          input("press enter to continue") 
+
+if de == "kde plasma minimal":
+          system("sudo pacman -S plasma-desktop konsole dolphin ark")
+          input("press enter to continue") 
+if de == "kde plasma": 
+          system("sudo pacman -S xorg xorg-xinit plasma plasma-wayland-session kde-applications")          
+          input("press enter to continue")  
+
+system("clear")
+                 
+kernel = input("zen kernel , lts kernel , xanmod kernel? = ")
+
+if kernel == "zen kernel":
+    system("sudo pacman -S linux-zen linux-zen-headers")
+    input("press enter to continue")
+    
+if kernel == "lts kernel":
+    system("sudo pacman -S linux-lts linux-lts-headers")
+    input("press enter to continue")
+    
+if kernel == "xanmod kernel":    
+    system("sudo pacman -S linux-xanmod linux-xanmod-headers")   
+    input("press enter to continue")
+    
+system("sudo grub-mkconfig -o /boot/grub/grub.cfg")
+input("press enter to continue")
+    
+system("clear")
+    
+gpu = input("amd , nvidia , intel , intel-nvidia? = ")
+
+if gpu == "amd":
+    system("sudo pacman -S --needed lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader")
+    input("press enter to continue")
+
+if gpu == "nvidia":
+    system("sudo pacman -S --needed nvidia-dkms nvidia-settings nvidia-utils opencl-nvidia primus_vk python-pycuda lib32-libvdpau lib32-nvidia-utils lib32-opencl-nvidia lib32-primus_vk python-glfw vkd3d lib32-vkd3d")
+    input("press enter to continue")
+
+if gpu == "intel":
+    system("sudo pacman -S --needed mesa lib32-mesa libva-intel-driver lib32-libva-intel-driver")
+    input("press enter to continue")
+
+if gpu == "intel-nvidia":
+    system("sudo pacman -S --needed mesa lib32-mesa libva-intel-driver lib32-libva-intel-driver nvidia-dkms nvidia-settings nvidia-utils opencl-nvidia primus_vk python-pycuda lib32-libvdpau lib32-nvidia-utils lib32-opencl-nvidia lib32-primus_vk python-glfw vkd3d lib32-vkd3d")
+    input("press enter to continue")    
+
+system("clear")
+
+from colorama import Fore, Back, Style
+print(Fore.RED + "if you want to install bbswitch write yes. If you dont want it write no.")
+print(Style.RESET_ALL)
+
+bbswitch = input("yes , no? = ")
+
+if bbswitch == "yes":
+    system("sudo pacman -S bbswitch-dkms")
+    input("press enter to continue") 
+    
+if bbswitch == "no":
+    input("press enter to continue")    
+
+system("clear")
+
+from colorama import Fore, Back, Style
+print(Fore.RED + "Now the necessary packages for the games will be installed.")
+print(Style.RESET_ALL)
+input("press enter to continue")
+
+system(f"{aur_helper} -S --needed lutris vkd3d wine-mono lib32-vkd3d wine-staging winetricks bottles protontricks-git protonup-qt wine-lol dxvk-bin steam steam-native-runtime")
+input("press enter to continue")
+system("sudo pacman -S --needed lib32-libldap giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo libxcomposite lib32-libxcomposite libxinerama lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader gamemode innoextract lib32-gamemode lib32-vkd3d vkd3d")
+
+system("clear")
+
+from colorama import Fore, Back, Style
+print(Fore.RED + "when winecfg is opened, go to libraries and add d3dcompiler47, d3d10 and d3d11. d3d11 and d3d10 is need to be native, d3dcompiler is need to be default.")
+print(Style.RESET_ALL)
+
+system("winecfg")
+input("press enter to continue")
+system("clear")
+input("press enter to continue")
